6d463f04884d67f4fe4d6d114e3ef50af6582df2
Furious_Locust <fluoride.amalgam@gmail.com>
2025-05-08 00:39:06 +0300

    Problems...

--- tests/test_system.sh
+++ tests/test_system.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 ar_dir="/dev/shm/AR_CL_Project_TMP_directory"
-gragen="gtg"
-gragen_src="gragen.cpp"
-check_equivalence="chequ"
-check_equivalence_src="check_equivalence.cpp"
 project_dir=$(pwd)
+
+required=("gragen" "check_equivalence" "dijkstra")
+
 testing_dirs=(0)
 test_case_count="0"
 
@@ -14,7 +13,7 @@ function run_tests {
 	for dir in ${testing_dirs[@]}; do
 		for (( test_case_num = 1; test_case_num <= $(($test_case_count)); test_case_num++ )) do
 			test_name="$ar_dir/tests/$dir/test_$test_case_num.in"
-			./$1 < $test_name > $ar_dir/results/$dir/$2_$test_case_num.out
+			$1 < $test_name > $ar_dir/results/$dir/$2_$test_case_num.out
 		done
 	done
 
@@ -28,18 +27,17 @@ function generate_tests {
 	mkdir -p $ar_dir/tests
 	mkdir -p $ar_dir/results
 
-	cp ./tests/$gragen_src $ar_dir/
-	cp ./tests/$check_equivalence_src $ar_dir/
-
-	clang++ -Wall -o $ar_dir/$gragen $ar_dir/$gragen_src -O2
-	clang++ -Wall -o $ar_dir/$check_equivalence $ar_dir/$check_equivalence_src -O2
+	for file in ${required[@]}; do
+		cp ./tests/$file.cpp $ar_dir/
+		clang++ -Wall -o $ar_dir/$file $ar_dir/$file.cpp -O2
+	done
 
 	for dir in ${testing_dirs[@]}; do
 		mkdir $ar_dir/tests/$dir
 		mkdir $ar_dir/results/$dir
 		for (( test_case_num = 1; test_case_num <= $(($test_case_count)); test_case_num++ )) do
 			test_name="$ar_dir/tests/$dir/test_$test_case_num.in"
-			$ar_dir/$gragen $test_case_num $dir > $test_name
+			$ar_dir/gragen $test_case_num $dir > $test_name
 			echo "Generation test $test_case_num with $dir vertexes is done."
 		done
 	done
@@ -57,12 +55,12 @@ else
 	read test_case_count testing_dirs < $ar_dir/cache
 fi
 
-echo -n "GPU "; { time run_tests "/build/app" "gpu"; } 2>&1 | grep real
-echo -n "CPU "; { time run_tests "/misc/dijkstra" "cpu"; } 2>&1 | grep real
+echo -n "GPU "; { time run_tests "./build/app" "gpu"; } 2>&1 | grep real
+echo -n "CPU "; { time run_tests "$ar_dir/dijkstra" "cpu"; } 2>&1 | grep real
 
 for dir in ${testing_dirs[@]}; do
 	for (( test_case_num = 1; test_case_num <= $(($test_case_count)); test_case_num++ )) do
-		$ar_dir/$check_equivalence $ar_dir/results/$dir/cpu_$test_case_count.out $ar_dir/results/$dir/gpu_$test_case_count.out
+		$ar_dir/check_equivalence $ar_dir/results/$dir/cpu_$test_case_count.out $ar_dir/results/$dir/gpu_$test_case_count.out
 	done
 done
 
